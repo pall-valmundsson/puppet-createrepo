036b31a2521f7da3a92dbb5f4dc2dc312d3b5d20
Pall Valmundsson <pall.valmundsson@gmail.com>
2013-05-13 21:24:32 +0000

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require 'rubygems'
-require 'puppetlabs_spec_helper/module_spec_helper'
 require 'coveralls'
 Coveralls.wear!
+require 'rubygems'
+require 'puppetlabs_spec_helper/module_spec_helper'
