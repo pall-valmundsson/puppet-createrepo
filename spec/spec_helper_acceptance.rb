86a381b52e812c468b8a3dd9f418ca3212b22b01
Pall Valmundsson <pall.valmundsson@gmail.com>
2014-05-29 12:43:42 +0000

    install stdlib on host in beaker tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -27,5 +27,8 @@ RSpec.configure do |c|
     c.before :suite do
         # Install module
         puppet_module_install(:source => proj_root, :module_name => 'createrepo')
+        hosts.each do |host|
+            on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+        end
     end
 end
